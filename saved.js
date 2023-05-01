58cff55bcc9e44e920e997d31a35b6a707544718
ImDarkTom <darktomg21@gmail.com>
2023-05-01 16:27:20 +0100

    updated saved

--- saved.js
+++ saved.js
@@ -1,23 +1,46 @@
-const params = new URLSearchParams(new URL(window.location.toLocaleString()).search);
-const subreddit = params.get('r');
-const query = params.get('q');
-
 let loopPos = -1;
 let currentVideos = JSON.parse(localStorage.getItem('saved')) || [];;
-let nextFullname = "";
 let nextThumbnail;
 let currentVideoData;
 
 const mainVid = document.querySelector('#main-vid');
+
 const vidTitle = document.querySelector('#vid-title');
+const vidSubreddit = document.querySelector('#subreddit-text');
+const vidAuthor = document.querySelector('#username-text');
+const vidAgo = document.querySelector('#time-ago-text');
+
+const upvoteText = document.querySelector('#upvote-amount');
+
 const nextVidThumb = document.querySelector('#next-vid-thumb');
+
+function getTimeAgo(utcTimestamp) {
+    const currentDate = new Date();
+    const timestampDate = new Date(utcTimestamp * 1000); // Convert to milliseconds
+
+    const timeDifference = currentDate.getTime() - timestampDate.getTime();
+    const secondsDifference = Math.round(timeDifference / 1000);
+
+    if (secondsDifference < 60) {
+        return "Just now";
+    } else if (secondsDifference < 3600) {
+        const minutes = Math.floor(secondsDifference / 60);
+        return `${minutes} ${minutes === 1 ? "minute" : "minutes"} ago`;
+    } else if (secondsDifference < 86400) {
+        const hours = Math.floor(secondsDifference / 3600);
+        return `${hours} ${hours === 1 ? "hour" : "hours"} ago`;
+    } else {
+        const days = Math.floor(secondsDifference / 86400);
+        return `${days} ${days === 1 ? "day" : "days"} ago`;
+    }
+}
 
 async function getNextVideo() {
     loopPos++;
-    const postData = currentVideos[loopPos].data;
+    const postData = currentVideos[loopPos];
     const redditMedia = postData.secure_media.reddit_video;
     currentVideoData = postData;
-    nextThumbnail = currentVideos[loopPos + 1].data.preview.images[0].source.url.replace(/amp;/g, '');
+    nextThumbnail = currentVideos[loopPos + 1].preview.images[0].source.url.replace(/amp;/g, '');
 
     if (redditMedia) {
         mainVid.src = redditMedia.fallback_url;
@@ -27,7 +50,17 @@ async function getNextVideo() {
 
     vidTitle.textContent = postData.title.replace(/amp;/g, '');
     vidTitle.href = "https://reddit.com" + postData.permalink;
-    searchInput.placeholder = postData.subreddit_name_prefixed;
+
+    vidSubreddit.textContent = postData.subreddit_name_prefixed;
+    vidSubreddit.href = `/?r=${postData.subreddit}`;
+
+    vidAuthor.textContent = `@${postData.author}`;
+    vidAuthor.href = `https://reddit.com/u/${postData.author}`;
+
+    upvoteText.textContent = postData.score < 1000 ? postData.score : `${(postData.score / 1000).toFixed(1)}K`;
+
+    vidAgo.textContent = `• ${getTimeAgo(postData.created)}`;
+
     mainVid.play();
 }
 
@@ -85,8 +118,8 @@ document.addEventListener('touchend', async function (e) {
         vidTitle.style = 'color: white;';
         nextVidThumb.style = `transform: translateY(150%);`;
         nextVidThumb.src = nextThumbnail;
-        const nextBorderHeight = (window.innerHeight/2) - (nextVidThumb.clientHeight/2);
-        nextVidThumb.style = `border-top: ${nextBorderHeight}px solid black; border-bottom: ${nextBorderHeight}px solid black;`;
+        const nextBorderHeight = (window.innerHeight/2) - (nextVidThumb.getBoundingClientRect().height/2) + nextVidThumb.getBoundingClientRect().top;
+        document.documentElement.style.setProperty('--next-border-height', `${nextBorderHeight}px`);
     }
 
     if (endY - startY >= 150) {
